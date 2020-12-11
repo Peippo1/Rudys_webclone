f022da169898d6d9e34ee9bb0b81d857780e7ca7
Peippo <32274695+Peippo1@users.noreply.github.com>
2020-12-11 15:26:00 +0000

    commit webclone inital files

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+// burger menu drop function
+jQuery(function($){
+    $( '.menu-btn' ).click(function(){
+        $('.responsive-menu').addClass('expand')
+        $('.menu-btn').addClass('btn-none')
+    })
+    
+     $( '.close-btn' ).click(function(){
+        $('.responsive-menu').removeClass('expand')
+        $('.menu-btn').removeClass('btn-none')
+    })
+  })
